fe2dabb049a20ac14c198ad27de3edc813c8ad41
Rishita <Rishita2010.be21@chitkara.edu.in>
2025-11-17 16:41:41 +0530

    Completed my js code

--- scripts.js
+++ scripts.js
@@ -1,13 +1,41 @@
 const degree = document.getElementById("tempdegree");
 const submitBtn = document.getElementById("submitBtn");
 
-const temprature = ""
-
 
 function handletemp(){
     const value = Number(degree.value);
 
     if(degree.value === "" || degree.value === null){
-        alert("Please enter your temperature")
+        alert("Please enter your temperature");
+        return;
+    }
+
+    //temp results
+    if(value < 0){
+        alert("It's extreamly cold outside");
+    }
+    else if(value < 16){
+        alert("It's cold outside");
+    }
+    else if(value<25){
+        alert("It's okay to go out");
+    }
+    else if(value<35){
+        alert("It's swim time");
+    }
+    else if(value<45){
+        alert("Can you please turn on the AC");
+    }
+    else{
+        alert("Too hot to handle");
     }
 }
+
+
+degree.addEventListener("keypress",function(event){
+    if(event.key === "Enter"){
+        handletemp();
+    }
+});
+
+submitBtn.addEventListener("click",handletemp);
